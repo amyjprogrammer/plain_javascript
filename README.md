8e66b39b2c3cf4af384b8b4073ca16ff476349a3
amyjprogrammer <amyjprogrammer@gmail.com>
2021-05-25 00:03:35 -0400

    Just JS to check age

---
+++ README.md
@@ -0,0 +1,6 @@
+***Working on Javascript without React and a tutorial***
+
+Specs
+-Javascript
+-HTML5
+-CSS
---
+++ static/js/script.js
@@ -0,0 +1,32 @@
+//Your Age in Days
+
+function ageInDays(){
+    var birthYear = prompt("What year were you born.. Good friend?");
+
+    //check if blank
+    if(birthYear !== null && birthYear !=""){
+
+        //get the current year
+        var date = new Date();
+        var currentYear = date.getFullYear();
+
+        //calculate days from the current year
+        var age = (currentYear - birthYear) * 365;
+    
+        //creating the Node and displaying the answer
+        var h2 = document.createElement('h2');
+        var textAnswer = document.createTextNode('You are ' + age + ' days old.');
+        h2.setAttribute('id', 'ageInDays');
+        h2.appendChild(textAnswer);
+        document.getElementById('flex-box-result').appendChild(h2); 
+    }     
+}
+
+//removing the text from textAnswer
+function reset(){
+    document.getElementById('ageInDays').remove();
+}
+
+
+
+
